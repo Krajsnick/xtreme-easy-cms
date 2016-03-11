17ba55b0708e180a8feb74bcf58944b8ee70cb5d
Robert Ahlberg <rob.ahlberg@gmail.com>
2016-03-11 16:35:02 +0100

    Temporary remove transition event handler

--- public/javascripts/site.js
+++ public/javascripts/site.js
@@ -1,14 +1,15 @@
 $(document).ready(function() {
 
-  $('#delete-button').click(function() {
-    $('article').css('transform', 'scale(0.01)');
-  });
-
-  $('article').on("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd", function() {
-    console.log("Working");
-    $(this).off()
-      .html('<img src="https://www.colourbox.com/preview/8503702-deleted-stamp.jpg">')
-      .css('transform', 'scale(1)');
-  });
+  // $('#delete-button').click(function() {
+  //   $('article').css('transform', 'scale(0.01)');
+  // });
+  //
+  // $('article').on("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd", function() {
+  //   if ($(this).is('article')) {
+  //     $(this).off()
+  //     .html('<img src="https://www.colourbox.com/preview/8503702-deleted-stamp.jpg">')
+  //     .css('transform', 'scale(1)');
+  //   }
+  // });
 
 });
